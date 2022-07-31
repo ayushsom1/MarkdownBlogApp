35d0aaadbc2e2b3d38522228b604466790f25409
ryzen <ryzensingh@gmail.com>
2022-08-01 01:05:28 +0530

    second commit

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const articles = [{
+        title: 'Test Article 1',
+        createdAt: new Date(),
+        description: 'Test Description 1'
+    }, {
+        title: 'Test Article 1',
+        createdAt: new Date(),
+        description: 'Test Description 2'
+    }]
+    res.render('index', { articles: articles })
 })
 
 app.listen(5000)
